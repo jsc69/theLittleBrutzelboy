399f542e1f71b701948c6452c811827fdf400b5d
jsc69 <jsc69@gmx.net>
2024-05-24 19:33:49 +0200

    first commit

---
+++ assets/tiles/Tree_medium.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tree_medium" tilewidth="8" tileheight="8" tilecount="24" columns="4">
+ <image source="PixelSpaces Free Pack/Backgrounds/Tree_medium.png" width="36" height="50"/>
+</tileset>
