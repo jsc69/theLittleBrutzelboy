3bd7bf4fefd2b3d82dd975eaa4b16cec2febb320
jsc69 <jsc69@gmx.net>
2024-05-26 00:47:23 +0200

    remove builds

---
+++ assets/tiles/gbs-mono.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="gbs-mono" tilewidth="8" tileheight="8" tilecount="224" columns="16">
+ <image source="../fonts/gbs-mono.png" width="128" height="112"/>
+</tileset>
