eed8fbbaf5e0b4598239c474787dd7d8b4a46678
jsc69 <jsc69@gmx.net>
2024-05-30 08:08:30 +0200

    add boxes

---
+++ assets/tiles/tb-logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tb-logo" tilewidth="8" tileheight="8" tilecount="378" columns="42">
+ <image source="../../tmp/tb-logo.png" width="343" height="77"/>
+</tileset>
