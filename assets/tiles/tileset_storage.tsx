25f48b2aa0140a7d18ca200a1527f2c260f09240
jsc69 <jsc69@gmx.net>
2024-06-08 20:18:58 +0200

    clean ups and finish setText

---
+++ assets/tiles/tileset_storage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_storage" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="tileset_storage.png" width="160" height="144"/>
+</tileset>
