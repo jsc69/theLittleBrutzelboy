a135f285eac69e38eeb4e920059a752ad38e5583
jsc69 <jsc69@gmx.net>
2024-06-16 14:42:40 +0200

    correct message for test

--- assets/tiles/theBrutzler.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="theBrutzler" tilewidth="8" tileheight="8" tilecount="4032" columns="42">
- <image source="../../../../../../Downloads/asserts/Free Animated Player v1.1/Free Animated Player.png" width="336" height="768"/>
-</tileset>
