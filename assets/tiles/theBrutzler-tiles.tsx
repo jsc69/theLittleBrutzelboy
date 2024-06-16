4e42b378a85e4bc8bffe4d3eba2056aa4ebc2e27
jsc69 <jsc69@gmx.net>
2024-06-16 14:19:37 +0200

    clean up, add checks for soldering

--- assets/tiles/theBrutzler-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="theBrutzler-tiles" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
- <image source="theBrutzler-tiles.png" width="256" height="256"/>
-</tileset>
--- assets/tiles/tileset.tsx
+++ assets/tiles/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+<tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
  <image source="../backgrounds/tileset.png" width="160" height="144"/>
 </tileset>
