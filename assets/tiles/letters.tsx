35cf3d9d22aa3b62e96a4313f9288d65483da8fd
jsc69 <jsc69@gmx.net>
2024-06-29 10:51:02 +0200

    selectable tutorial mode

---
+++ assets/tiles/letters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="!letters" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../tilesets/!letters.png" width="160" height="144"/>
+</tileset>
--- assets/tiles/tileset.tsx
+++ assets/tiles/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
- <image source="../backgrounds/tileset.png" width="160" height="144"/>
+ <image source="../tilesets/tileset.png" width="160" height="144"/>
 </tileset>
