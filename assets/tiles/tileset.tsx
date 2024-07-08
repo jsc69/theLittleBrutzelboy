e155d26e242928e42c3877f585e0202aececd93f
jsc69 <jsc69@gmx.net>
2024-07-08 19:45:55 +0200

    solder for all parts

--- assets/tiles/tileset.tsx
+++ assets/tiles/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
- <image source="../tilesets/tileset.png" width="160" height="144"/>
+<tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="tileset.png" width="256" height="256"/>
 </tileset>
